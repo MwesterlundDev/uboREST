21e98e471fd748488060b2562b1462d5279e4b92
Mark Westerlund <mark.westerlund.dev@gmail.com>
2015-01-01 13:37:25 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+
+// BASE SETUP
+// =============================================================
+
+//call the packages we need
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
