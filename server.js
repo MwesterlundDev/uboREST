01c8d27c1d90d2085b4c4fa1deea4fadfa17dff6
Kirstin Kennedy <Kirstin@Kirstins-MacBook-Pro.local>
2015-01-02 13:16:03 -0500

    started server.js

--- server.js
+++ server.js
@@ -7,3 +7,32 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
+//configure app to use bodyParser();
+//this will let us get the data from a POST
+
+app.use(bodyParser.urlecoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 1989 // set our port (tribute to the first year!!!)
+
+
+// ROUTES FORE OUR API
+// ==============================================================
+var router = express.router();
+
+// test route to make sure everything is working (accessed at GET ....)
+router.get('/', function(request, response){
+	res.json({message: 'horray! welcome to our api!'});
+
+});
+
+// more routes for our api will happen here
+
+// RESGISTER OUR ROUTES -----------------------------------------
+// all of our routes will be prefixed with /ubo
+app.use('/ubo', router);
+
+//START THE server
+app.listen(port);
+console.log("The magic happens on port " + port);
